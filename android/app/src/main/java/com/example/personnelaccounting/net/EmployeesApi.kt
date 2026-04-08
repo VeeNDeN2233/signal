068d1df6c042595571ebee1664f693985eb3b2f3
VeeNDeN2233 <belenikin2048@mail.ru>
2026-04-08 22:34:00 +0300

    /

--- android/app/src/main/java/com/example/personnelaccounting/net/EmployeesApi.kt
+++ android/app/src/main/java/com/example/personnelaccounting/net/EmployeesApi.kt
@@ -2,12 +2,27 @@ package com.example.personnelaccounting.net
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.PUT
 
 data class FcmTokenRequest(val fcm_token: String)
 
+data class EmployeeMeData(
+    val id: Int,
+    val last_name: String,
+    val first_name: String,
+    val middle_name: String?,
+    val unit_name: String?,
+    val position_name: String?,
+    val rank_name: String?
+)
+
+data class EmployeeMeResponse(val data: EmployeeMeData?)
+
 interface EmployeesApi {
+    @GET("/api/employees/me")
+    fun getMe(): Call<EmployeeMeResponse>
+
     @PUT("/api/employees/me/fcm-token")
     fun updateFcmToken(@Body body: FcmTokenRequest): Call<Any>
 }
-
